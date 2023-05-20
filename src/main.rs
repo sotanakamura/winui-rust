ca6975501d48c5373a4869335c795b5313bc2817
nakamura sota <sota.n_100527@icloud.com>
2023-05-21 00:39:18 +0900

    Add controls

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::cell::RefCell;
+use std::ops::ControlFlow;
+use windows::UI::Xaml::GridLengthHelper;
 use windows::core::IInspectable;
 use windows::core::implement;
 use windows::Foundation::Collections::IVector;
 use windows::UI::Xaml::Interop::TypeName;
+use windows_app::Microsoft::UI::Xaml::ThicknessHelper;
 use windows_app::bootstrap;
 use windows_app::Microsoft::UI::Xaml::Application;
 use windows_app::Microsoft::UI::Xaml::IApplicationOverrides;
@@ -19,6 +22,8 @@ use windows_app::Microsoft::UI::Xaml::Controls::ToggleSplitButton;
 use windows_app::Microsoft::UI::Xaml::Markup::XmlnsDefinition;
 use windows_app::Microsoft::UI::Xaml::Markup::IXamlMetadataProvider;
 use windows_app::Microsoft::UI::Xaml::XamlTypeInfo::XamlControlsXamlMetaDataProvider;
+use windows_app::Microsoft::UI::Xaml::Controls;
+use windows_app::Microsoft::UI::Xaml::Controls::Primitives;
 
 #[implement(IApplicationOverrides,IXamlMetadataProvider)]
 struct App {
@@ -41,7 +46,71 @@ impl IApplicationOverrides_Impl for App {
     fn OnLaunched(&self, _: &Option<LaunchActivatedEventArgs>) -> windows::core::Result<()> {
         Application::Current()?.Resources()?.MergedDictionaries()?.Append(XamlControlsResources::new()?);
         let window = Window::new()?;
-        window.SetContent(ColorPicker::new()?);
+
+        let grid = Controls::Grid::new()?;
+        let col1 = Controls::ColumnDefinition::new()?;
+        col1.SetMaxWidth(200f64);
+        let col2 = Controls::ColumnDefinition::new()?;
+        grid.ColumnDefinitions()?.Append(col1);
+        grid.ColumnDefinitions()?.Append(col2);
+
+        let stackpanel = Controls::StackPanel::new()?;
+        stackpanel.SetValue(Controls::Grid::ColumnProperty()?,IInspectable::try_from(0)?);
+
+        let button = Controls::Button::new()?;
+        button.SetContent(IInspectable::try_from("Button")?);
+        button.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(button);
+        
+        let dropdownbutton = Controls::DropDownButton::new()?;
+        dropdownbutton.SetContent(IInspectable::try_from("DropDownButton")?);
+        dropdownbutton.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(dropdownbutton);
+
+        let togglebutton = Controls::Primitives::ToggleButton::new()?;
+        togglebutton.SetContent(IInspectable::try_from("ToggleButton")?);
+        togglebutton.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(togglebutton);
+        
+        let splitbutton = Controls::SplitButton::new()?;
+        splitbutton.SetContent(IInspectable::try_from("SplitButton")?);
+        splitbutton.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(splitbutton);
+
+        let togglesplitbutton = Controls::ToggleSplitButton::new()?;
+        togglesplitbutton.SetContent(IInspectable::try_from("ToggleSplitButton")?);
+        togglesplitbutton.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(togglesplitbutton);
+
+        let combobox = Controls::ComboBox::new()?;
+        combobox.Items()?.Append(IInspectable::try_from("ComboBox")?);
+        combobox.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(combobox);
+
+        let radiobutton = Controls::RadioButton::new()?;
+        radiobutton.SetContent(IInspectable::try_from("RadioButton")?);
+        radiobutton.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(radiobutton);
+
+        let ratingcontrol = Controls::RatingControl::new()?;
+        ratingcontrol.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(ratingcontrol);
+
+        let slider = Controls::Slider::new()?;
+        slider.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(slider);
+
+        let toggleswitch = Controls::ToggleSwitch::new()?;
+        toggleswitch.SetMargin(ThicknessHelper::FromUniformLength(10f64)?);
+        stackpanel.Children()?.Append(toggleswitch);
+
+        let colorpicker = Controls::ColorPicker::new()?;
+        colorpicker.SetValue(Controls::Grid::ColumnProperty()?,IInspectable::try_from(1)?);
+        //Controls::Grid::SetColumn(colorpicker, 1);
+
+        grid.Children()?.Append(stackpanel);
+        grid.Children()?.Append(colorpicker);
+        window.SetContent(grid);
 
         let result = window.Activate();
         *self.window.borrow_mut() = Some(window);
